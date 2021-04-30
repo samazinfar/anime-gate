6d6b63bdd5ee35f5259e2b2083a8cd561fb8f190
Sam Azinfar <azinfar05@gmail.com>
2021-04-30 10:45:43 +0200

    insertion de l'horloge sur la page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Horloge2 from "./components/Horloge2";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Horloge2 />
     </div>
   );
 }
