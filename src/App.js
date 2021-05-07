14e9b53a096c2721213e35a8ee53a85ffa9020e8
Dumitru <ru.dumit@gmail.com>
2021-05-07 10:56:53 +0200

    fixe  error navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Switch, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Home from "./components/Home";
 import ResponsiveNavBar from "./components/ResponsiveNavBar";
 import AnimeDetails from "./components/AnimeDetails";
